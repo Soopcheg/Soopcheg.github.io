40ca0e27677a00ef133ec04730014496d8124a62
Soopcheg <42252145+Soopcheg@users.noreply.github.com>
2019-04-28 00:14:47 +0300

    Add files via upload

--- land/script.js
+++ land/script.js
@@ -6,6 +6,8 @@ $('#convers').ionRangeSlider({
     from: 3, // предустановленное значение ОТ
     type: "single", // тип слайдера
     step: 5, // шаг слайдера
+    // grid: "true",
+    // grid_snap: "true",
     skin: "round"
 });
 
@@ -15,6 +17,8 @@ $('#budget').ionRangeSlider({
     from: 60000, // предустановленное значение ОТ
     type: "single", // тип слайдера
     step: 20000, // шаг слайдера
+    // grid: "true",
+    // grid_snap: "true",
     skin: "round"
 });
 
@@ -24,13 +28,39 @@ $('#click').ionRangeSlider({
     from: 10, // предустановленное значение ОТ
     type: "single", // тип слайдера
     step: 100, // шаг слайдера
+    // grid: "true",
+    // grid_snap: "true",
     skin: "round"
 });
 
-var width=screen.width;
+var convers,
+    budget,
+    click,
+    now,
+    withbot,
+    dif;
 
-if (width<=768){
-    $(".call").attr("href", "https://api.whatsapp.com/send?phone=745333355566")
+var values = $(".uselanding form .irs-single");
+
+window.onload = function () {
+    $(".uselanding form").change(function (e) {
+        convers = parseFloat(values["0"].innerText);
+        budget = parseFloat(values["1"].innerText) * 1000;
+        click = parseFloat(values["2"].innerText);
+        now = budget / click * convers / 100;
+        withbot = budget / click * convers / 10;
+        dif = withbot - now;
+        $("#requests__now").text(now);
+        $("#requests__withbot").text(withbot);
+        $(".requests__dif").text(dif + "заявок");
+        console.log(budget + " " + convers + " " + click)
+        console.log(now + " " + withbot + " " + dif)
+    });
+
+    var width = screen.width;
+
+        if (width <= 768) {
+            $("footer .call").attr("href", "https://api.whatsapp.com/send?phone=745333355566")
+        }
+
 }
-
-// console.log(width);
