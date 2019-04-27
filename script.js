1175034ccfefba300d6f292bc19dc0765fb57432
Soopcheg <42252145+Soopcheg@users.noreply.github.com>
2019-04-27 21:41:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict'
+
+$('#convers').ionRangeSlider({
+    min: 0, // минимальное значение
+    max: 30, // максимальное значение
+    from: 3, // предустановленное значение ОТ
+    type: "single", // тип слайдера
+    step: 5, // шаг слайдера
+    skin: "round"
+});
+
+$('#budget').ionRangeSlider({
+    min: 0, // минимальное значение
+    max: 200000, // максимальное значение
+    from: 60000, // предустановленное значение ОТ
+    type: "single", // тип слайдера
+    step: 20000, // шаг слайдера
+    skin: "round"
+});
+
+$('#click').ionRangeSlider({
+    min: 0, // минимальное значение
+    max: 500, // максимальное значение
+    from: 10, // предустановленное значение ОТ
+    type: "single", // тип слайдера
+    step: 100, // шаг слайдера
+    skin: "round"
+});
+
+var width=screen.width;
+
+if (width<=768){
+    $(".call").attr("href", "https://api.whatsapp.com/send?phone=745333355566")
+}
+
+// console.log(width);
